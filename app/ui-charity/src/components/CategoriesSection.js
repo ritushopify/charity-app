e41b202ccfab7a37d9e13f3b0a23a561108bb3e9
Ritu Sikka <ritu.sikka@shopify.com>
2019-12-05 21:06:08 -0500

    took a long time to separate the querygit add .

--- app/ui-charity/src/components/CategoriesSection.js
+++ app/ui-charity/src/components/CategoriesSection.js
@@ -1,33 +1,19 @@
 import React, { useCallback } from "react";
-import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
-
 import { Navigation } from "@shopify/polaris";
 import { HomeMajorMonotone } from "@shopify/polaris-icons";
-
-const CAT_QUERY = gql`
-  query {
-    categories {
-      categoryId
-      description
-    }
-  }
-`;
+import { categoryQuery } from "./CategoryQuery";
 
 export default function CategoriesSection() {
-  const { loading, error, data } = useQuery(CAT_QUERY);
+  const { loading, error, data } = useQuery(categoryQuery);
   if (loading) return "Loading...";
-  if (error) {
-    console.log("error is " + error);
-    return `Error! ${error.message}`;
-  }
-  console.log("data is" + data.categories);
-  const items = data.categories.map(category => {
+  if (error) return `Error! ${error.message}`;
+  const categories = data.categories;
+  const items = categories.map(category => {
     return {
       url: "charities",
       label: category.description,
       icon: HomeMajorMonotone
-      //    onClick: selectedCategory(category.categoryId)
     };
   });
   return <Navigation.Section separator title="Categories" items={items} />;
